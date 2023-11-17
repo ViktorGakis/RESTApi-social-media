39d75e1907f156008dcb9089bf68c4065f305d9a
ViktorGakis <victor.g.gakis@gmail.com>
2023-11-17 23:23:17 +0200

    big logging config plus rotating file handler

--- storeapi/logging_conf.py
+++ storeapi/logging_conf.py
@@ -13,21 +13,38 @@ def configure_logging() -> None:
                     "class": "logging.Formatter",
                     "datefmt": "%Y-%m-%dT%H:%M:%S",
                     "format": "%(name)s:%(lineno)d - %(message)s",
-                }
+                },
+                "file": {
+                    "class": "logging.Formatter",
+                    "datefmt": "%Y-%m-%dT%H:%M:%S",
+                    "format": "%(asctime)s.%(msecs)03dZ | %(levelname)-8s | %(name)s:%(lineno)d - %(message)s",
+                },
             },
             "handlers": {
                 "default": {
                     "class": "rich.logging.RichHandler",
                     "level": "DEBUG",
                     "formatter": "console",
-                }
+                },
+                "rotating_file": {
+                    "class": "logging.handlers.RotatingFileHandler",
+                    "level": "DEBUG",
+                    "formatter": "file",
+                    "filename": "storeapi.log",
+                    # 1MB
+                    "maxBytes": 1024 * 1024,
+                    # 2 MB in total,
+                    "backupCount": 2,
+                    # best and more compact for english
+                    "encoding": "utf8",
+                },
             },
             "loggers": {
-                "uvicorn": {"handlers": ["default"], "level": "INFO"},
+                "uvicorn": {"handlers": ["default", "rotating_file"], "level": "INFO"},
                 # root is the parent of all
                 # storeapi.routers.post
                 "storeapi": {
-                    "handlers": ["default"],
+                    "handlers": ["default", "rotating_file"],
                     "level": "DEBUG" if isinstance(config, DevConfig) else "INFO",
                     # root is out
                     # we do not want to use it in dev
