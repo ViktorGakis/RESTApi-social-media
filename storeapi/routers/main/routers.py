c2c446a00c2af0826a9f540a519877798e346d28
ViktorGakis <victor.g.gakis@gmail.com>
2023-11-17 18:29:23 +0200

    added comments

--- storeapi/routers/main/routers.py
+++ storeapi/routers/main/routers.py
@@ -1,10 +1,19 @@
+from fastapi import HTTPException, status
+
+from storeapi.models.post import Comment, CommentIn, UserPostWithComments
+
 from ...models import UserPost, UserPostIn
 from . import router
 
 post_table: dict = {}
+comment_table: dict = {}
 
 
-@router.post("/", response_model=UserPost)
+def find_post(post_id: int):
+    return post_table.get(post_id)
+
+
+@router.post("/post", response_model=UserPost, status_code=status.HTTP_201_CREATED)
 async def create_post(post: UserPostIn):
     data = post.model_dump()
     last_record_id = len(post_table)
@@ -16,3 +25,43 @@ async def create_post(post: UserPostIn):
 @router.get("/post", response_model=list[UserPost])
 async def get_all_posts():
     return list(post_table.values())
+
+
+@router.post("/comment", response_model=Comment, status_code=status.HTTP_201_CREATED)
+async def create_comment(comment: CommentIn):
+    post = find_post(comment.post_id)
+    if not post:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail="Post not found."
+        )
+    data = comment.model_dump()
+    last_record_id = len(comment_table)
+    new_comment = {**data, "id": last_record_id}
+    comment_table[last_record_id] = new_comment
+    return new_comment
+
+
+@router.get(
+    "/post/{post_id}/comments",
+    response_model=list[Comment],
+    status_code=status.HTTP_200_OK,
+)
+async def get_comments_on_post(post_id: int):
+    return [
+        comment for comment in comment_table.values() if comment["post_id"] == post_id
+    ]
+
+
+@router.get(
+    "/post/{post_id}",
+    response_model=UserPostWithComments,
+    status_code=status.HTTP_200_OK,
+)
+async def get_post_with_comments(post_id: int):
+    post = find_post(post_id)
+    if not post:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail="Post not found"
+        )
+
+    return {"post": post, "comments": await get_comments_on_post(post_id)}
