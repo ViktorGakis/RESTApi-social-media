32308b7b4bbdfc7d3dc58aa0b60049beeea0234f
ViktorGakis <victor.g.gakis@gmail.com>
2023-11-18 02:51:16 +0200

    user retrieval and jwt decode + tests

--- storeapi/security.py
+++ storeapi/security.py
@@ -2,7 +2,8 @@ import logging
 from datetime import UTC, datetime, timedelta
 
 from fastapi import HTTPException, status
-from jose import jwt
+from jose import ExpiredSignatureError, jwt
+from jose.exceptions import JWEError
 from passlib.context import CryptContext
 
 from .db import database, user_table
@@ -15,7 +16,9 @@ ALGORITHM = "HS256"
 pwd_context = CryptContext(schemes=["bcrypt"])
 
 credentials_exception = HTTPException(
-    status_code=status.HTTP_401_UNAUTHORIZED, detail="Could not validate credentials"
+    status_code=status.HTTP_401_UNAUTHORIZED,
+    detail="Could not validate credentials",
+    headers={"WWW-Authenticate": "Bearer"},
 )
 
 
@@ -53,5 +56,30 @@ async def authenticate_user(email: str, password: str):
     if not user:
         raise credentials_exception
     if not verify_password(password, user.password):
+        raise credentials_exception
+    return user
+
+
+async def get_current_user(token: str):
+    try:
+        payload = jwt.decode(
+            token,
+            key=SECRET_KEY,
+            algorithms=ALGORITHM,
+            # options={"verify_signature": True},
+        )
+        email = payload.get("sub")
+        if email is None:
+            raise credentials_exception
+    except ExpiredSignatureError as e:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Token has expired",
+            headers={"WWW-Authenticate": "bearer"},
+        ) from e
+    except JWEError as e:
+        raise credentials_exception from e
+    user = await get_user(email=email)
+    if user is None:
         raise credentials_exception
     return user
